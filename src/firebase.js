63d64859deb93dbe20ab68a973029dfa42b1f3f6
Your Name <deudajm@gmail.com>
2025-06-15 00:07:36 +0800

    `Added dotenv and updated dependencies, removed hardcoded Firebase API keys, and added environment variables to .gitignore`

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0OygZQqq03pp4BlCAOAmdwNReR2WrGSA",
-  authDomain: "sk-messaging-ce99c.firebaseapp.com",
-  projectId: "sk-messaging-ce99c",
-  storageBucket: "sk-messaging-ce99c.firebasestorage.app",
-  messagingSenderId: "304951243302",
-  appId: "1:304951243302:web:0e7e729b4abaed61caece7",
-  measurementId: "G-BFV1H7PZ9M",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app
