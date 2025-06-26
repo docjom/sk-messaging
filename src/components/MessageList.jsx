eed3eac9d0590984937a327693512f6d14010fb3
Your Name <deudajm@gmail.com>
2025-06-26 20:18:35 +0800

    `Refactor message list component: add emoji reactions and update timestamp display`

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -458,7 +458,7 @@ export const MessageList = ({
         {!isImage && !isVideo && (
           <div
             className={`border relative px-3 pt-3 bg-gray-50 mb-2 ${
-              message.message !== "" ? "rounded-t-lg pb-3" : "rounded-lg pb-5"
+              message.message !== "" ? "rounded-t-lg pb-3" : "rounded-lg"
             }`}
           >
             <div className="flex items-center space-x-3">
@@ -486,55 +486,125 @@ export const MessageList = ({
                 <Icon icon="solar:download-bold" width="16" height="16" />
               </Button>
             </div>
-            {!message.message && (
-              <div
-                className={`absolute bottom-1 right-1 border-gray-200/50 border backdrop-blur-sm rounded-full max-w-32 gap-1 px-2 ${
-                  message.senderId === user.uid
-                    ? "justify-end bg-gray-500/50"
-                    : "justify-start bg-gray-500/20"
-                }`}
-              >
-                <div className="flex items-center space-x-2">
-                  <p
-                    className={`text-xs py-0.5 ${
-                      message.senderId === user.uid
-                        ? "text-white/70"
-                        : "text-gray-800"
-                    }`}
-                  >
-                    {formatTimestamp(message.timestamp)}
-                  </p>
-                  {message.senderId === user.uid && (
-                    <div className="flex">
-                      {message.senderId === user.uid && (
-                        <div className="flex">
-                          {message.status === "sending" ? (
-                            <Icon
-                              icon="ic:round-access-time"
-                              width="14"
-                              height="14"
-                              className="animate-pulse"
-                            />
-                          ) : message.status === "sent" && !message.seen ? (
-                            <Icon
-                              icon="ic:round-check"
-                              width="16"
-                              height="16"
-                            />
-                          ) : message.seen ? (
-                            <Icon
-                              icon="solar:check-read-linear"
-                              width="20"
-                              height="20"
-                            />
-                          ) : null}
-                        </div>
-                      )}
-                    </div>
-                  )}
+            <div className="flex items-center gap-1.5">
+              {/* emoji reactions to message */}
+              {!message.message && (
+                <div className="relative max-w-30 pb-0.5 px-1 my-1">
+                  <div className="flex gap-1 pr-4 justify-start items-center max-w-30 overflow-x-auto  scrollbar-hide">
+                    {message.reactions && (
+                      <>
+                        {message.reactions &&
+                          Object.entries(message.reactions).map(
+                            ([emojiSrcSet, users]) => (
+                              <span
+                                key={emojiSrcSet}
+                                className={`flex gap-1 justify-start items-center border rounded-full  px-1 py-0.5 ${
+                                  message.senderId === user.uid
+                                    ? "border-gray-300"
+                                    : "bg-gray-200/50"
+                                }`}
+                              >
+                                <span className="rounded-full bg-gray-200/50 size-5">
+                                  <picture className="cursor-pointer">
+                                    <source
+                                      srcSet={`https://fonts.gstatic.com/s/e/notoemoji/latest/${emojiSrcSet}/512.webp`}
+                                      type="image/webp"
+                                    />
+                                    <img
+                                      src={`https://fonts.gstatic.com/s/e/notoemoji/latest/${emojiSrcSet}/512.gif`}
+                                      alt=""
+                                      width="32"
+                                      height="32"
+                                    />
+                                  </picture>
+                                </span>
+
+                                <div className="*:data-[slot=avatar]:ring-background flex -space-x-2 *:data-[slot=avatar]:ring-1 ">
+                                  {/* Map over users array for this emoji */}
+                                  {users.slice(0, 3).map((user) => (
+                                    <Avatar
+                                      key={user.userId}
+                                      className="h-5 w-5"
+                                    >
+                                      <AvatarImage
+                                        src={
+                                          getSenderData(user.userId)?.photoURL
+                                        }
+                                        alt={`@${user.userId}`}
+                                      />
+                                      <AvatarFallback>
+                                        {user.userId
+                                          .substring(0, 2)
+                                          .toUpperCase()}
+                                      </AvatarFallback>
+                                    </Avatar>
+                                  ))}
+
+                                  {/* Show +X if more than 3 users */}
+                                  {users.length > 3 && (
+                                    <div className="h-5 w-5 rounded-full bg-gray-300 flex items-center justify-center text-xs font-medium text-gray-600">
+                                      +{users.length - 3}
+                                    </div>
+                                  )}
+                                </div>
+                              </span>
+                            )
+                          )}
+                      </>
+                    )}
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
+              {!message.message && (
+                <div
+                  className={` border-gray-200/50 border backdrop-blur-sm rounded-full max-w-32 gap-1 px-2 ${
+                    message.senderId === user.uid
+                      ? "justify-end bg-gray-500/50"
+                      : "justify-start bg-gray-500/20"
+                  }`}
+                >
+                  <div className="flex items-center space-x-2">
+                    <p
+                      className={`text-xs py-0.5 ${
+                        message.senderId === user.uid
+                          ? "text-white/70"
+                          : "text-gray-800"
+                      }`}
+                    >
+                      {formatTimestamp(message.timestamp)}
+                    </p>
+                    {message.senderId === user.uid && (
+                      <div className="flex">
+                        {message.senderId === user.uid && (
+                          <div className="flex">
+                            {message.status === "sending" ? (
+                              <Icon
+                                icon="ic:round-access-time"
+                                width="14"
+                                height="14"
+                                className="animate-pulse"
+                              />
+                            ) : message.status === "sent" && !message.seen ? (
+                              <Icon
+                                icon="ic:round-check"
+                                width="16"
+                                height="16"
+                              />
+                            ) : message.seen ? (
+                              <Icon
+                                icon="solar:check-read-linear"
+                                width="20"
+                                height="20"
+                              />
+                            ) : null}
+                          </div>
+                        )}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         )}
 
